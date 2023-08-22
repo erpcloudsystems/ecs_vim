ebddddc855cdd5c8c8dbd299c9cbb9ada8a046ba
erpcloud.systems <erpcloud.systems@gmail.com>
2023-08-22 17:50:10 +0200

    cleaning jsons

---
+++ ecs_vim/ecs_vim/report/costcenterwise_pnl/costcenterwise_pnl.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, ERPCloud.systems and contributors
+# For license information, please see license.txt
+
+# import frappe
+
+
+def execute(filters=None):
+	columns, data = [], []
+	return columns, data
